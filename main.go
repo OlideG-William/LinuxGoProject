a471d643573927720004386ab489204b0ff26668
Falcon <FalconStrix@gmail.com>
2023-11-21 23:05:38 +0200

    Http serve

--- main.go
+++ main.go
@@ -1,38 +1,26 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net/http"
-	"time"
 )
 
-func fif() {
+func fif(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
 
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
 }
 
 func main() {
-	statrtime := time.Now()
+	http.HandleFunc("/hello", fif)
+	http.HandleFunc("/headers", headers)
 
-	resp, err := http.Get("http://gobyexample.com")
-	if err != nil {
-		panic(err)
-	}
+	http.ListenAndServe(":8090", nil)
 
-	for i := 0; i < 10; i++ {
-		fmt.Println("Hello")
-	}
-
-	defer resp.Body.Close()
-	fmt.Println("Response status:", resp.Status)
-	scanner := bufio.NewScanner(resp.Body)
-	for i := 0; scanner.Scan() && i < 5; i++ {
-		fmt.Println(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Compile time program: ", time.Since(statrtime))
 }
